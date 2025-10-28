33b1c6a5db684208f1899055b1c27c36bf936701
Ozodbek <ozodbekmaxammatov1@gmail.com>
2025-10-28 20:15:01 +0200

    added order_item path to urlpattern

--- store/urls.py
+++ store/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
 	path('cart/', views.cart, name="cart"),
 	path('checkout/', views.checkout, name="checkout"),
     path('order/', views.order_confirm, name="order"),
-    path('order_item/', views.order_item, name="order_item")
+    path('product/<int:pk>/', views.order_item, name='order_item'),
 
 ]
